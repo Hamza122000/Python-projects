b4232f188d23f1c1c1b157135011d9f779e19a77
Hamza122000 <76016840+Hamza122000@users.noreply.github.com>
2020-12-15 12:18:56 -0600

    Add files via upload

---
+++ hw1/hw1.py
@@ -0,0 +1,120 @@
+# Hamza Benkhalifa:
+
+import turtle, platform
+
+#TODO: Fill out the Purpose, Input Parameter(s), and Return Value
+# for each of the two functions below in comments
+
+#Remember, you must place a # before any comment, or it will be
+# interpreted as Python code, and will probably cause errors.
+
+#Part 1: cents
+#==========================================
+# Purpose: 
+# The purpose of this program is to calculate to total value of a given amount of money
+# Input Parameter(s): The parameters are: quarters, Dimes, Nickels, and cents
+#   
+# Return Value:
+# the return value is the value of all the total quarters,dimes,nickels, and pennies added up and converted into pennies  
+#==========================================
+
+def cents(quarters, dimes, nickels, pennies):
+    total = 0
+    total += quarters*25
+    total += dimes*10
+    total += nickels*5
+    total += pennies
+    return total
+
+#Part 2: draw_M
+#==========================================
+# Purpose: The purpose of this code is to draw the latter M using code
+#   
+# Input Parameter(s):
+#None
+# Return Value: 
+# The letter M in Ywllow and Maroon
+#==========================================
+
+def draw_M():
+    turtle.delay(0)
+    turtle.bgcolor("gold")
+    turtle.hideturtle()
+    turtle.color("maroon")
+    turtle.penup()
+    turtle.setpos(-200,-100)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.forward(120)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.right(120)
+    turtle.forward(80)
+    turtle.right(120)
+    turtle.forward(28)
+    turtle.right(120)
+    turtle.forward(14)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(128)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(14)
+    turtle.right(120)
+    turtle.forward(28)
+    turtle.right(120)
+    turtle.forward(80)
+    turtle.right(120)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(120)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(28)
+    turtle.right(60)
+    turtle.forward(140)
+    turtle.right(120)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(120)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.right(120)
+    turtle.forward(52)
+    turtle.right(120)
+    turtle.forward(52)
+    turtle.right(120)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(120)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.right(120)
+    turtle.forward(140)
+    turtle.right(60)
+    turtle.forward(28)
+    turtle.left(90)
+    turtle.forward(64)
+    turtle.end_fill()
+
+vers = platform.python_version()
+assert vers[0] == '3', "You must use Python 3, "+vers+" is not acceptable"
+
+
+
+
